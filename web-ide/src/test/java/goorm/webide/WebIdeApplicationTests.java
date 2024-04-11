e43db82c0b79f3bdc80468f5c892e231a4186527
ExpeditionMoon <dnjswon1025@gmail.com>
2024-04-11 16:55:42 +0900

    프로젝트 파일 초기 세팅
    프로젝트 파일 초기 세팅

---
+++ web-ide/src/test/java/goorm/webide/WebIdeApplicationTests.java
@@ -0,0 +1,13 @@
+package goorm.webide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebIdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

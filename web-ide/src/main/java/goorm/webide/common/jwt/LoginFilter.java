e5c6a0e660051a3506ff31eafa0df2e332c51a8d
leejuno <zhqkfxm0127@naver.com>
2024-04-19 20:42:53 +0900

    refactor: 회원가입시 form데이터를 받도록 수정 #8

---
+++ web-ide/src/main/java/goorm/webide/common/jwt/LoginFilter.java
@@ -0,0 +1,2 @@
+package goorm.webide.common.jwt;public class LoginFilter {
+}
--- web-ide/src/main/java/goorm/webide/user/controller/UserController.java
+++ web-ide/src/main/java/goorm/webide/user/controller/UserController.java
@@ -10,9 +10,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -23,7 +22,7 @@ public class UserController {
     private final UserService service;
 
     @PostMapping("/user/register")
-    public ResponseEntity registerUser(@Valid @RequestBody RegisterRequest registerRequest,
+    public ResponseEntity registerUser(@Valid @ModelAttribute RegisterRequest registerRequest,
                                        BindingResult bindingResult
     ){
         validateAllParam(passwordValidate(registerRequest, bindingResult));
